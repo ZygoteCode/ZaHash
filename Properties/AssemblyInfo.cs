ce4360524ff556cef317344ba0850f8ad62bb898
Gabriele Bologna <bolognagabriele2003@gmail.com>
2022-08-16 20:52:07 +0200

    First commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ZaHash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GabryB03")]
+[assembly: AssemblyProduct("ZaHash")]
+[assembly: AssemblyCopyright("Copyright © GabryB03 2022")]
+[assembly: AssemblyTrademark("Copyright © GabryB03 2022")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("38b2eafb-8945-4638-9ad7-c18a452318f4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
